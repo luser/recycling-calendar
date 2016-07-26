77a982d92c18587577fbb2aba71dd4eac2d0ff83
Ted Mielczarek <ted@mielczarek.org>
2016-07-26 09:43:10 -0400

    add CORS header

--- webapp.py
+++ webapp.py
@@ -10,5 +10,10 @@ from genrecyclingcalendar import make_recycling_calendar
 ONE_YEAR = int(datetime.timedelta(365).total_seconds())
 
 def application(env, start_response):
-    start_response('200 OK', [('Content-Type', 'text/calendar'), ('Cache-Control', 'max-age=%d' % ONE_YEAR)])
+    start_response('200 OK',
+                   [
+                       ('Content-Type', 'text/calendar'),
+                       ('Cache-Control', 'max-age=%d' % ONE_YEAR),
+                       ('Access-Control-Allow-Origin', '*'),
+                   ])
     return [make_recycling_calendar()]
